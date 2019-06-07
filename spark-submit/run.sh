bda045cd725f78cd5632425b6e9ca9cc9090e074
Victor Santiago <vitongos@users.noreply.github.com>
2019-06-07 11:23:35 +0200

    Update run.sh

--- spark-submit/run.sh
+++ spark-submit/run.sh
@@ -8,5 +8,5 @@ do
 	esac
 done
 
-docker run --rm --network dockersparkcluster_spark-network -p "4040:4040" --env APP="$APP" spark-submit:2.3.1
+docker run --rm --network mllib-src_spark-network -p "4040:4040" --env APP="$APP" spark-submit:2.3.1
 ## -v "./samples/source":/opt/spark-apps -v "./samples/data":/opt/spark-data
